2be3aa6f95a20fe47a7989ab430f1a8b6c00a8cb
scopichol <scopichol@gmail.com>
2017-10-28 21:27:15 +0300

    Lesson 2 Add event

--- OffChainDebt.sol
+++ OffChainDebt.sol
@@ -1,13 +1,17 @@
 pragma solidity ^0.4.15;
 
 contract OffChainDebt {
-    address owner;
+    address public owner;
     mapping(address => uint) public debt;
     
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
+    
+    event TakeDebt(address debter, uint amount);
+    event RepayDebt(address debter, uint amount);
+    
     function OffChainDebt() public {
         owner = msg.sender;
     }
@@ -15,10 +19,12 @@ contract OffChainDebt {
     function take(uint amount) public {
         require(debt[msg.sender] + amount >= debt[msg.sender]);
         debt[msg.sender] += amount;
+        TakeDebt(msg.sender, amount);
     }
     
     function repay(address debtor, uint amount) public onlyOwner {
         require(debt[debtor] - amount <= debt[debtor]);
         debt[debtor] -= amount;
+        RepayDebt(debtor, amount);
     }
 }
